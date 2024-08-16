54cbec13cfba7f84cfbf03b8ce27c04fd441e4ab
JoaoVitorNSouza <joao.souza.nascimento1@gmail.com>
2024-08-16 06:10:12 -0400

    Versão Atualizada

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib import auth
 
 
 # Create your views here.
@@ -41,3 +42,26 @@ def cadastro(request):
         messages.add_message(request, constants.SUCCESS,
                              'Usuário cadastrado com sucesso.')
         return redirect('/usuarios/logar')
+
+
+def logar(request):
+    if request.method == 'GET':
+        return render(request, 'logar.html')
+
+    elif request.method == 'POST':
+        # Pegar os dados.
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        # Validações se usuário existe e se as senhas conferem.
+        user = auth.authenticate(username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/empresarios/cadastrar_empresa')
+
+        else:
+            messages.add_message(request, constants.ERROR,
+                                 'Usuário ou senhas inválidas.')
+
+            return redirect('/empresarios/cadastrar_empresa')
